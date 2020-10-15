5c2f6a30b79181d370a9c8a81327f05f432b27dc
Ariel I <arielibarra39@gmail.com>
2020-10-15 02:43:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,19 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Hello, World"
-  end 
-  
-  get '/name' do 
-    "My name is #{name}"
-  end 
-  
-  get '/hometown' do 
-    "My hometown is #{hometown}"
-  end 
-  
-  get '/favorite-song' do 
-    "My favorite song is #{favorite_song}"
-  end 
+    "Started my server using Shotgun!"
+  end
 
 end
